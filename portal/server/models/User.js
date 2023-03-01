67650676b6a7c7b430e800f359fd48b929167b8d
Isaiah411013 <Archangel_r320@hotmail.com>
2023-02-28 21:06:37 -0800

    building out the server side of new prtal

---
+++ portal/server/models/User.js
@@ -0,0 +1,48 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/(?i)^.+@patriotfire\.com$/, 'Email must be @patriotfire.com'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+
+    next();
+});
+
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+userSchema.virtual('bookCount').get(function () {
+    return this.savedBooks.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
